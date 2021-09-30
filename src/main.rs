f95e7f63b9d1d58da60e600c246a6d17d8f730ed
Jan Algermissen <algermissen@acm.org>
2021-09-30 17:16:09 +0200

    Try custom filter

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,25 @@
 use std::convert::Infallible;
-use warp::Filter;
+use warp::{Filter, Rejection};
 
-#[derive(Clone,Debug)]
+#[derive(Clone,Debug,Copy)]
 struct Validator;
-
-fn _validate(vld: Validator, val: i32) -> Result<String, Infallible> {
-    Ok(format!("Validator '{:?}' says '{}' is valid.", vld, val))
-}
 
 async fn async_validate(vld: Validator, val: i32) -> Result<String, Infallible> {
     Ok(format!("Validator '{:?}' says '{}' is valid.", vld, val))
 }
 
+fn validated_thing(validator:Validator) -> impl Filter<Extract = (String,), Error = Rejection> + Copy {
+    warp::path::param().and_then(move |v:i32| {
+            async_validate(validator,v)
+        })
+}
 
 
 #[tokio::main]
 async fn main() {
     let validator = Validator;
 
-    let route = warp::path!("test" / i32)
-        .and_then(move |v| {
-            async_validate(validator.clone(),v)
-        })
+    let route = warp::path("test").and(validated_thing(validator))
         .map(|value| format!("Result: {}", value));
 
     warp::serve(route).run(([127, 0, 0, 1], 3030)).await;
