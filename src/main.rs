10a4143b442379f80727c924af8bd14dc884e934
Jan Algermissen <algermissen@acm.org>
2021-09-30 18:44:23 +0200

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use std::convert::Infallible;
 use warp::{Filter, Rejection};
 
-#[derive(Clone,Debug,Copy)]
+#[derive(Clone,Debug)]
 struct Validator;
 
 async fn async_validate(vld: Validator, val: i32) -> Result<String, Infallible> {
     Ok(format!("Validator '{:?}' says '{}' is valid.", vld, val))
 }
 
-fn validated_thing(validator:Validator) -> impl Filter<Extract = (String,), Error = Rejection> + Copy {
-    warp::path::param().and_then(move |v:i32| {
-            async_validate(validator,v)
+fn validated_thing(validator:Validator) -> impl Filter<Extract = (String,), Error = Rejection> + Clone{
+    warp::any().map(move || validator.clone()).and(warp::path::param()).and_then(move |vld:Validator, v:i32| {
+            async_validate(vld,v)
         })
 }
 
